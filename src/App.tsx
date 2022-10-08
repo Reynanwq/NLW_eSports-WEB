1822d08c0ab45b88e080c95056ce7205845bccd5
Reynan Paiva <reynanwq@gmail.com>
2022-10-08 16:34:38 -0300

    add data on the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
-function App() {
- return <h1>Helllo NLW</h1>
+interface ButtonProps{
+    title: string;
+}
+
+function Button(props: ButtonProps){
+    return(
+        <button>
+            {props.title}
+        </button>
+    )
+}
+
+function App(){
+    return(
+        <div>
+            <Button title="Send 1"/>
+            <Button title="Send 2"/>
+            <Button title="Send 3"/>
+            <Button title="Hello World"/>
+        </div>
+    )
 }
 
 export default App
