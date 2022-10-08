4c400e0994f8c3f1be92ca2283651cee630889ba
Reynan Paiva <reynanwq@gmail.com>
2022-10-08 17:29:35 -0300

    deleted file styles.css + creat postcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-interface ButtonProps{
-    title: string;
-}
+import './styles/main.css';
 
 function App(){
     return(
-        <div>
+        <div className="w-8 h-8 bg-black dark:bg-slate-500">
         </div>
     )
 }
