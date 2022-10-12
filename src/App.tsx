ee93f772c023dc0f4ddace8dda62e6ec1f7700e2
Reynan Paiva <reynanwq@gmail.com>
2022-10-12 11:26:08 -0300

    font-inter + newPreview + LandingPage-finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { MagnifyingGlassPlus } from 'phosphor-react'
+
 import './styles/main.css';
 
 import logoImg from "./assets/nlw-esports-logo.svg";
@@ -13,42 +15,42 @@ function App(){
         <div className="grid grid-cols-6 gap-6 mt-16">
             <a href="relative rounded-lg overflow-hidden">
                 <img src="/game-1.png" alt="" />
-            <div className="w-full pt-16 pb-4 px-4 bg-game-gradient bottom-0 left-0 right-0">
+            <div className="w-full pt-2 pb-4 px-4 bg-game-gradient bottom-0 left-0 right-0">
                 <strong className="font-bold text-white block overflow-hidden h-4 text-sm">League of Legends</strong>
                 <span className="text-zinc-300 text-sm block mt-1 overfolw">4 Anuncios</span>
             </div>
             </a>
             <a href="relative rounded-lg overflow-hidden">
                 <img src="/game-2.png" alt="" />
-                <div className="w-full pt-16 pb-4 px-4 bg-game-gradient bottom-0 left-0 right-0">
+                <div className="w-full pt-2 pb-4 px-4 bg-game-gradient bottom-0 left-0 right-0">
                 <strong className="font-bold text-white block overflow-hidden h-4 text-sm">DOTA 2</strong>
                 <span className="text-zinc-300 text-sm block mt-1 overfolw">4 Anuncios</span>
             </div>
             </a>
             <a href="relative rounded-lg overflow-hidden">
                 <img src="/game-3.png" alt="" />
-                <div className="w-full pt-16 pb-4 px-4 bg-game-gradient bottom-0 left-0 right-0">
+                <div className="w-full pt-2 pb-4 px-4 bg-game-gradient bottom-0 left-0 right-0">
                 <strong className="font-bold text-white block overflow-hidden h-4 text-sm">Counter-Strike</strong>
                 <span className="text-zinc-300 text-sm block mt-1 overfolw">4 Anuncios</span>
             </div>
             </a>
             <a href="relative rounded-lg overflow-hidden">
                 <img src="/game-4.png" alt="" />
-                <div className="w-full pt-16 pb-4 px-4 bg-game-gradient bottom-0 left-0 right-0">
-                <strong className="font-bold text-white block overflow-hidden h-4 text-sm">Apex Legenhs</strong>
+                <div className="w-full pt-2 pb-4 px-4 bg-game-gradient bottom-0 left-0 right-0">
+                <strong className="font-bold text-white block overflow-hidden h-4 text-sm">Apex Legends</strong>
                 <span className="text-zinc-300 text-sm block mt-1 overfolw">4 Anuncios</span>
             </div>
             </a>
             <a href="relative rounded-lg overflow-hidden">
                 <img src="/game-5.png" alt="" />
-                <div className="w-full pt-16 pb-4 px-4 bg-game-gradient bottom-0 left-0 right-0">
+                <div className="w-full pt-2 pb-4 px-4 bg-game-gradient bottom-0 left-0 right-0">
                 <strong className="font-bold text-white block overflow-hidden h-4 text-sm">Fortinite</strong>
                 <span className="text-zinc-300 text-sm block mt-1 overfolw">4 Anuncios</span>
             </div>
             </a>
             <a href="relative">
                 <img src="/game-6.png" alt="" />
-                <div className="w-full pt-16 pb-4 px-4 bg-game-gradient bottom-0 left-0 right-0">
+                <div className="w-full pt-2 pb-4 px-4 bg-game-gradient bottom-0 left-0 right-0">
                 <strong className="font-bold text-white block overflow-hidden h-4 text-sm">World of Warcraft</strong>
                 <span className="text-zinc-300 text-sm block mt-1 overfolw">4 Anuncios</span>
             </div>
@@ -60,7 +62,8 @@ function App(){
                     <strong className="text-2xl text-white font-black block">Não encontrou o seu duo?</strong>
                     <span className="text-zinc-400 block">Publique um anuncio para encontrar novos players!</span>
                 </div>
-            <button className="py-3 px-4 bg-violet-500 hover:bg-violet-800 text-white rounded">
+            <button className="py-3 px-4 bg-violet-500 hover:bg-violet-800 text-white rounded flex items-center gap-2">
+                <MagnifyingGlassPlus size={24}/>
                 Publicar Anúncio
             </button>
             </div>
